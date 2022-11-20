6ae543c0e2a21203409d4f829a36d2a49363c315
BOMEN <bomen21x@gmail.com>
2022-11-20 16:03:55 +0700

    Update __manifest__.py

--- ctp_fonts_th/__manifest__.py
+++ ctp_fonts_th/__manifest__.py
@@ -18,7 +18,7 @@
     "description": """ 
             Add Fonts Thai to Document Layout
             .""",
-    "author": "Cybernetics Plus Co., Ltd.",
+    "author": "Cybernetics+",
     "website": "https://www.cybernetics.plus",
     "live_test_url": "https://www.cybernetics.plus",
     "images": ["static/description/banner.gif"],
